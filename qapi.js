42f0748f8ae74538ffe4cfcffd427a217b227116
Timofey Bobkov <bobkov.star.tima@gmail.com>
2024-11-09 00:43:47 +0200

    Add files via upload

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"672e85e141a0cf0024a55da8",
+				"672e85e141a0cf0024a55dab"
+			]
+		},
+		"672e85e141a0cf0024a55da8": {
+			"id": "672e85e141a0cf0024a55da8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"672e85e141a0cf0024a55dab": {
+			"id": "672e85e141a0cf0024a55dab",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
